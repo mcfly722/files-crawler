ad3d9fbfda18926de5e4829ae00bc4c2cc004731
mcfly722 <7221798@gmail.com>
2024-11-23 23:55:00 +0300

    try/catch for unhandled errors

--- reviewFolders.ps1
+++ reviewFolders.ps1
@@ -1,6 +1,6 @@
 param(
     $connectionString,
-    $batchSize=5,
+    $batchSize=100,
     $initialRoot='c:\'
 )
 
@@ -15,23 +15,27 @@ Open-MySqlConnection -ConnectionName "sql" -ConnectionString $connectionString
 Invoke-SQLQuery -ConnectionName "sql" -query "CALL reportFolder(@folderPath, @exist);" -parameters @{'folderPath'=$initialRoot;'exist'=1}
 
 do {
-    $foldersToCheck = Invoke-SQLQuery -ConnectionName "sql" -query "CALL getNextFoldersForReview(@batchSize);" -parameters @{'batchSize'=50}
+    try {
+        Write-Host "getNextFoldersForReview" -ForegroundColor Green
+        $foldersToCheck = Invoke-SQLQuery -ConnectionName "sql" -query "CALL getNextFoldersForReview(@batchSize);" -parameters @{'batchSize'=$batchSize}
 
-    foreach($folder in $foldersToCheck){
-        write-host $folder.fullPath
-        if (test-path $folder.fullPath) {
-            try {
-                Get-ChildItem -Directory $folder.fullPath -ErrorAction Stop | ForEach-Object {
-                    write-host $_.FullName
-                    Invoke-SQLQuery -ConnectionName "sql" -query "CALL reportFolder(@folderPath, @exist);" -parameters @{'folderPath'=$_.FullName;'exist'=1} 
+        foreach($folder in $foldersToCheck){
+            write-host $folder.fullPath
+            if (test-path $folder.fullPath) {
+                try {
+                    Get-ChildItem -Directory $folder.fullPath -ErrorAction Stop | ForEach-Object {
+                        write-host $_.FullName
+                        Invoke-SQLQuery -ConnectionName "sql" -query "CALL reportFolder(@folderPath, @exist);" -parameters @{'folderPath'=$_.FullName;'exist'=1} 
+                    }
+                } catch {
+                    Invoke-SQLQuery -ConnectionName "sql" -query "CALL reportFolder(@folderPath, @exist);" -parameters @{'folderPath'=$folder.fullPath;'exist'=2}
                 }
-            } catch {
-                Invoke-SQLQuery -ConnectionName "sql" -query "CALL reportFolder(@folderPath, @exist);" -parameters @{'folderPath'=$folder.fullPath;'exist'=2}
+            } else {
+                # folder does not exist any more (deleted)
+                Invoke-SQLQuery -ConnectionName "sql" -query "CALL reportFolder(@folderPath, @exist);" -parameters @{'folderPath'=$folder.fullPath;'exist'=0}
             }
-        } else {
-            # folder does not exist any more (deleted)
-            Invoke-SQLQuery -ConnectionName "sql" -query "CALL reportFolder(@folderPath, @exist);" -parameters @{'folderPath'=$folder.fullPath;'exist'=0}
         }
+    } catch {
+        Write-Host "$($PSItem.ToString())" -ForegroundColor Red
     }
-    Start-Sleep -Seconds 1
 } while ($true)
